1cfc414ade4a386cc85f250ef1d96e16a0540cf5
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2021-01-27 12:13:59 +0530

    Added logger middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+// function logger(obj, next, action)
+// logger(obj)(next)(action)
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log("ACTION_TYPE = ", action.type);
+      next(action);
+    };
+  };
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("store: ", store);
 // console.log("BEFORE STATE: ", store.getState());
 //
@@ -19,7 +31,7 @@ console.log("store: ", store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App store={store}/>
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById("root")
 );
